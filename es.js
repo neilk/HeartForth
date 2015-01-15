139043383f2085f960d84f169ed5a69958f9d9d9
Neil Kandalgaonkar <neilk@brevity.org>
2015-01-15 01:12:21 -0800

    almost working

--- es.js
+++ es.js
@@ -1,20 +1,66 @@
-var readline = require('readline'),
-    rl = readline.createInterface(process.stdin, process.stdout);
+var readline = require('readline');
+var forth = require('./lib/jsforth');
+forth.init();
+forth._print = console.log;
 
-rl.setPrompt('OHAI> ');
+process.stdin.setEncoding('utf8');
+process.stdout.setEncoding('utf8');
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+
+
+function getSymbols(string) {
+  var length = string.length;
+  var index = -1;
+  var output = [];
+  var character;
+  var charCode;
+  while (++index < length) {
+    character = string.charAt(index);
+    charCode = character.charCodeAt(0);
+    if (charCode >= 0xD800 && charCode <= 0xDBFF) {
+      // Note: this doesn't account for lone high surrogates;
+      // you'd need even more code for that!
+      output.push(character + string.charAt(++index));
+    } else {
+      output.push(character);
+    }
+  }
+  return output;
+}
+
+
+rl.setPrompt('> ');
 rl.prompt();
 
 rl.on('line', function(line) {
-  switch(line.trim()) {
-    case 'hello':
-      console.log('world!');
-      break;
-    default:
-      console.log('Say what? I might have heard `' + line.trim() + '`');
-      break;
-  }
+  line = line.trim();
+  //var symbols = getSymbols(line);
+  //console.log(symbols.join(' '));
+  forth._run(line);
   rl.prompt();
 }).on('close', function() {
   console.log('Have a great day!');
   process.exit(0);
 });
+
+/*
+var prompt = require('prompt');
+
+//
+// Start the prompt
+//
+prompt.start();
+
+//
+// Get two properties from the user: username and email
+//
+prompt.get(['username', 'email'], function (err, result) {
+  //
+  // Log the results.
+  //
+  console.log('Command-line input received:');
+  console.log('  username: ' + result.username);
+  console.log('  email: ' + result.email);
+});
+*/
