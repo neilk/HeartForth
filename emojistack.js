fa114e2559c5948ad46bd2ddd69e908f57ab6301
Neil Kandalgaonkar <neilk@brevity.org>
2015-01-14 21:32:36 -0800

    first pass

---
+++ emojistack.js
@@ -0,0 +1,20 @@
+var readline = require('readline'),
+    rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('OHAI> ');
+rl.prompt();
+
+rl.on('line', function(line) {
+  switch(line.trim()) {
+    case 'hello':
+      console.log('world!');
+      break;
+    default:
+      console.log('Say what? I might have heard `' + line.trim() + '`');
+      break;
+  }
+  rl.prompt();
+}).on('close', function() {
+  console.log('Have a great day!');
+  process.exit(0);
+});
